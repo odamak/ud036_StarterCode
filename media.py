efe6aea506548548ef7302aab932f348abeafceb
Omar Damak <omardamak1@gmail.com>
2017-06-11 11:35:39 +0200

    Correcting comment on media.py

--- media.py
+++ media.py
@@ -9,6 +9,6 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    #method to open web browser with given trailer url
+    #Method to open web browser with given trailer url
     def show_trailer (self):
         webbrowser.open(self.trailer_youtube_url)
